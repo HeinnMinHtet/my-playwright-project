5ad98bb32cd3ab2a2e01482d021e18f9e104a354
heinminhtet-hmh <emobrain.hmhh@gmail.com>
2026-04-01 19:18:40 +0700

    Initial commit: Playwright setup with Apple Silicon CI

---
+++ tests/e2e/generated.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://phyueducation.com/');
+  await page.locator('div').filter({ hasText: 'PHYU EDUCATION AND CONSULTANT' }).first().click();
+  await page.getByRole('link').filter({ hasText: /^$/ }).nth(3).click();
+  await page.getByRole('link', { name: 'Login Page' }).click();
+  await page.getByRole('textbox', { name: 'Your username or email' }).click();
+  await page.getByRole('textbox', { name: 'Your username or email' }).fill('hwllcbcone@gamcom');
+  await page.locator('form > div:nth-child(2)').first().click();
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('helloworld');
+  await page.getByRole('link', { name: 'Log In' }).click();
+  await page.getByText('Home Login Page').click();
+});
---
+++ tests/e2e/sqli.spec.js
@@ -0,0 +1,14 @@
+import {test, expect} from '@playwright/test';
+
+test('SQL Injection test - login bypass attempt', async ({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/login');
+
+    const payload = `' OR '1'='1`;
+
+    await page.fill('#username', payload);
+    await page.fill('#password', payload);
+    await page.click('button[type=submit]');
+
+    //Expect login to FAIL (secure behaviour)
+    await expect(page.locator('#flash')).toContainText('Your username is invalid!');
+});
