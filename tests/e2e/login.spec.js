6695591d6871abc7fe3982f445dbbcb47fc49be6
heinminhtet-hmh <emobrain.hmhh@gmail.com>
2026-04-02 20:09:08 +0700

    refactor login test using data-driven approach

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,5 +1,8 @@
 import { test, expect } from '@playwright/test';
 
+const URL = 'https://the-internet.herokuapp.com/login';
+
+/*
 test('login success', async ({page}) => {
     await page.goto('https://the-internet.herokuapp.com/login');
 
@@ -9,3 +12,26 @@ test('login success', async ({page}) => {
 
     await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
 });
+*/
+
+const users = [
+  { username: 'tomsmith', password: 'SuperSecretPassword!', expected: 'success' },
+  { username: 'tomsmith', password: 'WrongPassword!', expected: 'fail' },
+  { username: '', password: '123', expected: 'fail' }
+];
+
+for (const user of users) {
+  test(`login test - ${user.username || 'empty'} - ${user.expected}`, async ({ page }) => {
+    await page.goto(URL);
+
+    await page.fill('#username', user.username);
+    await page.fill('#password', user.password);
+    await page.click('button[type="submit"]');
+
+    if (user.expected === 'success') {
+        await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+    } else {
+        await expect(page.locator('#flash')).toContainText('invalid');
+    }
+  });
+}
--- tests/e2e/security-login.spec.js
+++ tests/e2e/security-login.spec.js
@@ -9,7 +9,7 @@ test('login success', async ({page}) => {
     await page.fill('#password', 'SuperSecretPassword!');
     await page.click('button[type="submit"]');
 
-    await expect(page.locator('#flash')).toContainText('You logged into a secure');
+    await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
 });
 
 test('login failed (wrong password)', async ({page}) => {
