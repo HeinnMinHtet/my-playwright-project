9ffdce1e4bded1f49b97c5cf0ae4f5ad2b59896b
heinminhtet-hmh <emobrain.hmhh@gmail.com>
2026-04-08 17:21:21 +0700

    Refactor Playwright tests with Page Object Model and beforeEach hook

---
+++ tests/pages/login.page.js
@@ -0,0 +1,23 @@
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+    this.usernameInput = '#username';
+    this.passwordInput = '#password';
+    this.loginButton = 'button[type="submit"]';
+    this.flashMessage = '#flash';
+  }
+
+  async goto(url) {
+    await this.page.goto(url);
+  }
+
+  async login(username, password) {
+    await this.page.fill(this.usernameInput, username);
+    await this.page.fill(this.passwordInput, password);
+    await this.page.click(this.loginButton);
+  }
+
+  getFlashMessage() {
+    return this.page.locator(this.flashMessage);
+  }
+}
