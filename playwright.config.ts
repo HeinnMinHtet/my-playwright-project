27dd1f94772628df9adbd30517896b3630d5c7f8
heinminhtet-hmh <emobrain.hmhh@gmail.com>
2026-04-23 17:53:06 +0700

    improve reporter output

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,14 +22,17 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   ...(process.env.CI && { workers: 1 }),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+            ['html'],
+            ['list']
+            ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: 'https://the-internet.herokuapp.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     screenshot: 'only-on-failure',// Capture screenshot on failure
     video: 'retain-on-failure',// Record video on failure
   },
